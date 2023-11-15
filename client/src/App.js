a3900bb51bbae2b9209706c4dde7240bab245895
Olena Zdanovska <freshjs@gmail.com>
2023-11-15 09:54:39 +0200

    Add lazy load

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import TodoPage from './pages/TodoPage';
-import UserPage from './pages/UserPage';
 import NavMenu from './components/NavMenu';
-import RegistrationPage from './pages/RegistrationPage';
-import UserProfile from './components/UserProfile';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const TodoPage = lazy(() => import('./pages/TodoPage'));
+const UserPage = lazy(() => import('./pages/UserPage'));
+const RegistrationPage = lazy(() => import('./pages/RegistrationPage'));
+const UserProfile = lazy(() => import('./components/UserProfile'));
+
+// import HomePage from './pages/HomePage';
+// import TodoPage from './pages/TodoPage';
+// import UserPage from './pages/UserPage';
+// import RegistrationPage from './pages/RegistrationPage';
+// import UserProfile from './components/UserProfile';
 
 const App = () => {
   return (
     <BrowserRouter>
       <NavMenu />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/todo" element={<TodoPage />} />
-        <Route path="/users" element={<UserPage />} />
-        <Route path="/users/:idUser" element={<UserProfile />} />
-        <Route path="/sign-up" element={<RegistrationPage />} />
-      </Routes>
+      <Suspense fallback={'Loading...'}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/todo" element={<TodoPage />} />
+          <Route path="/users" element={<UserPage />} />
+          <Route path="/users/:idUser" element={<UserProfile />} />
+          <Route path="/sign-up" element={<RegistrationPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
